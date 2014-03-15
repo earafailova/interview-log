7f3088d5dbddd34cf5e7fbf998811e230c60bef1
Arafailova E <earafailova@gmail.com>
2014-03-15 22:24:21 +0700

    some tables added

---
+++ interview-log/Models/TagModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace interview_log.Models
+{
+    public class Tag 
+    {
+        [Key] public string Name { get; set; }
+        public string Info { get; set; }
+    }
+
+    public class TagsDbContext : DbContext
+    {
+        public TagsDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
