b197b3a55b7d19f3fcc70b7c7467cc39f451e697
akhramov <futu.fata@gmail.com>
2014-03-18 16:47:34 +0700

    merge

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(interview_log.Startup))]
+namespace interview_log
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
