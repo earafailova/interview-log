d605a6e23c40a7eb50e6032f845182a3493a478f
akhramov <futu.fata@gmail.com>
2014-03-25 17:03:06 +0700

    fix

---
+++ interview-log/Migrations/201403250802011_init.cs
@@ -0,0 +1,130 @@
+namespace interview_log.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        UserName = c.String(),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        Email = c.String(),
+                        Position = c.String(),
+                        State = c.Byte(),
+                        Admin = c.Boolean(),
+                        Interviewer = c.Boolean(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Attachments",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        DownloadsCount = c.Int(nullable: false),
+                        Type = c.Byte(nullable: false),
+                        Name = c.String(),
+                        Size = c.String(),
+                        UserId = c.String(maxLength: 128),
+                        FullPath = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        User_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.User_Id, cascadeDelete: true)
+                .Index(t => t.User_Id);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.LoginProvider, t.ProviderKey })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.RoleId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Comments",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        AuthorId = c.String(),
+                        Author = c.String(),
+                        Date = c.DateTime(nullable: false),
+                        Text = c.String(),
+                        User_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.User_Id)
+                .Index(t => t.User_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Comments", "User_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "User_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Attachments", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.Comments", new[] { "User_Id" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "User_Id" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.Attachments", new[] { "UserId" });
+            DropTable("dbo.Comments");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.Attachments");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.AspNetRoles");
+        }
+    }
+}
---
+++ interview-log/Models/Comment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace interview_log.Models
+{
+    public class Comment
+    {
+        public Comment() { }
+        public Comment(string author, string text)
+        {
+            this.AuthorId = author;
+            this.Text = text;
+            this.Date = DateTime.Now;
+            ApplicationDbContext db = new ApplicationDbContext();
+            this.Author = db.Users.Find(author).UserName;
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; private set; }
+        public string AuthorId { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+    }
+}
